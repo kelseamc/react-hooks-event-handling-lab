fa6385010aaa9bb9bb4c35b112b693708ed5f7b9
kelseamc <kelsea.mcall@gmail.com>
2021-01-13 13:57:03 -0500

    commit tree 4563c82d05330b6bde6cd404e4f4679f60e8a436

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+function Keypad(){
+
+    return(
+        <input onChange={() => console.log("Entering password...")}type="password" placeholder="Password"/> 
+
+    )
+
+}
+
+export default Keypad
